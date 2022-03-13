54f2aa6ca2fc0b580c045698b6988f6aa2d400c1
Thiago Cechetto <>
2022-03-12 16:21:25 -0800

    Create Entry Screen UI https://trello.com/c/35D0IPtS

--- app/src/main/java/com/example/trainschedule/MainActivity.kt
+++ app/src/main/java/com/example/trainschedule/MainActivity.kt
@@ -1,25 +1,40 @@
 package com.example.trainschedule
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.DateRange
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.Dimension
+import androidx.fragment.app.FragmentActivity
 import com.example.trainschedule.ui.theme.TrainScheduleTheme
+import com.example.trainschedule.ui.theme.Typography
+import com.google.android.material.datepicker.MaterialDatePicker
+import java.text.SimpleDateFormat
+import java.util.*
 
-class MainActivity : ComponentActivity() {
+class MainActivity : FragmentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             TrainScheduleTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background,
+                    ) {
+                    BuildUi()
                 }
             }
         }
@@ -27,14 +42,178 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun BuildUi() {
+    Box(
+        modifier = Modifier
+            .padding(all = 24.dp)
+    ) {
+        ConstraintLayout(
+            modifier = Modifier.fillMaxSize()
+        ) {
+            val (column, button) = createRefs()
+            Column(
+                verticalArrangement = Arrangement.Center,
+                modifier = Modifier
+                    .padding(bottom = 64.dp)
+                    .constrainAs(column) {
+                        top.linkTo(parent.top)
+                        bottom.linkTo(parent.bottom)
+                        start.linkTo(parent.start)
+                        end.linkTo(parent.end)
+                    },
+            ) {
+                HeaderText(text = "From:")
+                DropdownView()
+                HeaderText(text = "To:")
+                DropdownView()
+                HeaderText(text = "When:")
+                DatePickerView(datePicked = "Select Date", updatedDate = {  })
+            }
+            Button(
+                onClick = { /*TODO*/ },
+                modifier = Modifier
+                    .constrainAs(button) {
+                        bottom.linkTo(parent.bottom)
+                        start.linkTo(parent.start)
+                        end.linkTo(parent.end)
+                        width = Dimension.fillToConstraints
+                    }
+            ) {
+                Text(text = "See Times")
+            }
+        }
+    }
+}
+
+@Composable
+fun HeaderText(
+    text: String,
+    modifier: Modifier = Modifier
+) {
+    Text(
+        text = text,
+        style = Typography.h1,
+        modifier = modifier.padding(
+            top = 32.dp,
+            bottom = 8.dp
+        )
+    )
+}
+
+@Composable
+fun DropdownView() {
+    val expanded = remember { mutableStateOf(false) }
+    val items = listOf("A", "B", "C", "D", "E", "F")
+    val selectedIndex = remember { mutableStateOf(0) }
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .wrapContentSize(Alignment.TopStart)
+            .border(0.5.dp, MaterialTheme.colors.onSurface.copy(alpha = 0.5f))
+            .clickable(onClick = { expanded.value = true })
+    ) {
+        Text(
+            items[selectedIndex.value], modifier =
+            Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+        )
+        DropdownMenu(
+            expanded = expanded.value,
+            onDismissRequest = { expanded.value = false },
+        ) {
+            items.forEachIndexed { index, s ->
+                DropdownMenuItem(onClick = {
+                    selectedIndex.value = index
+                    expanded.value = false
+                }) {
+                    Text(text = s)
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun DatePickerView(
+    datePicked: String?,
+    updatedDate: (date: Long?) -> Unit,
+    activity: FragmentActivity = LocalContext.current as FragmentActivity
+) {
+    val selectedDateValue = remember { mutableStateOf(datePicked) }
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .wrapContentSize(Alignment.TopStart)
+//            .padding(top = 10.dp, bottom = 64.dp)
+            .border(0.5.dp, MaterialTheme.colors.onSurface.copy(alpha = 0.5f))
+            .clickable {
+                showDatePicker(activity) {
+                    updatedDate(it)
+                    it?.let {
+                        selectedDateValue.value = it.toStringDate()
+                    }
+                }
+            }
+    ) {
+
+        ConstraintLayout(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+        ) {
+
+            val (label, iconView) = createRefs()
+
+            Text(
+                text = selectedDateValue.value ?: "Date Picker",
+                color = MaterialTheme.colors.onSurface,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .constrainAs(label) {
+                        top.linkTo(parent.top)
+                        bottom.linkTo(parent.bottom)
+                        start.linkTo(parent.start)
+                        end.linkTo(iconView.start)
+                        width = Dimension.fillToConstraints
+                    },
+            )
+
+            Icon(
+                imageVector = Icons.Default.DateRange,
+                contentDescription = null,
+                modifier = Modifier
+                    .size(20.dp, 20.dp)
+                    .constrainAs(iconView) {
+                        end.linkTo(parent.end)
+                        top.linkTo(parent.top)
+                        bottom.linkTo(parent.bottom)
+                    },
+                tint = MaterialTheme.colors.onSurface
+            )
+
+        }
+
+    }
+}
+
+private fun showDatePicker(
+    activity: FragmentActivity,
+    updatedDate: (Long?) -> Unit
+) {
+    val picker = MaterialDatePicker.Builder.datePicker().build()
+    picker.show(activity.supportFragmentManager, picker.toString())
+    picker.addOnPositiveButtonClickListener {
+        updatedDate(it)
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     TrainScheduleTheme {
-        Greeting("Android")
+        BuildUi()
     }
 }
+
+fun Long.toStringDate(): String = SimpleDateFormat("MM/dd/yyyy", Locale.US).format(this)
--- app/src/main/java/com/example/trainschedule/ui/theme/Color.kt
+++ app/src/main/java/com/example/trainschedule/ui/theme/Color.kt
@@ -2,6 +2,9 @@ package com.example.trainschedule.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
+val Red1 = Color(0xFFFF0000)
+val Red2 = Color(0xFFBB2222)
+///
 val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
